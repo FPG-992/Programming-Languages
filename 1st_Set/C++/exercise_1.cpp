39f098234c4fac09ba189b282184434dc8ad8f14
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-04-16 15:19:53 +0300

    Changed the file exercise_1.cpp | Variables

--- 1st_Set/C++/exercise_1.cpp
+++ 1st_Set/C++/exercise_1.cpp
@@ -26,26 +26,26 @@ int main(int argc, char* argv[]){
         }
     }
 
-    long long totalSum = prefix_sum[N]; //total sum of the sequence, last element of the prefix sum
-    long long minSum = totalSum; //initialize the minSum with the total sum, because the maximum difference can't be bigger than the total sum
+    long long TotalSum = prefix_sum[N]; //total sum of the sequence, last element of the prefix sum
+    long long MinimumSum = TotalSum; //initialize the MinimumSum with the total sum, because the maximum difference can't be bigger than the total sum
 
     //solution of (O(n^2)) | 2 FOR LOOPS
     /* Nested loop to check for all possible subsequence sums and non-subsequence sums
-    
+
     */
 
     for (int i=1; i<=N; i++){ 
         for (int j=i; j<N; j++){ 
-            long long subsequence_sum = prefix_sum[j] - prefix_sum[i-1]; //calculate the subsequence sum
-            long long non_subsequence_sum = totalSum - subsequence_sum; //calculate the non-subsequence sum
-            long long difference = abs(subsequence_sum - non_subsequence_sum); //calculate the difference
-            if (difference < minSum){ //if the difference is smaller than the minSum, update the minSum
-                minSum = difference; //update the minSum
+            long long subsequence_total_sum = prefix_sum[j] - prefix_sum[i-1]; //calculate the subsequence sum
+            long long non_subsequence_total_sum = TotalSum - subsequence_total_sum; //calculate the non-subsequence sum
+            long long difference = abs(subsequence_total_sum - non_subsequence_total_sum); //calculate the difference
+            if (difference < MinimumSum){ //if the difference is smaller than the MinimumSum, update the MinimumSum
+                MinimumSum = difference; //update the MinimumSum
             }
         }
     }
 
-    cout<<minSum<<endl; //print the minSum
+    cout<<MinimumSum<<endl; //print the MinimumSum
 
     //close the file
     file.close();
