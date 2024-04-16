fb80281c78a2ae1b08233ac2182fa6767d08d5b3
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-04-16 14:47:51 +0300

    Added comments

--- 1st_Set/C++/exercise_1.cpp
+++ 1st_Set/C++/exercise_1.cpp
@@ -15,13 +15,13 @@ int main(int argc, char* argv[]){
     
     file>>N;
 
-    vector<int> sequence(N);    
-    vector<long long> prefix_sum(N+1,0); 
+    vector<int> sequence(N); //initialize the sequence with N elements
+    vector<long long> prefix_sum(N+1,0); //initialize the prefix sum with 0
 
-    while(!file.eof()){
-        for(int i=0; i<N; i++){
-            file>>sequence[i];
-            prefix_sum[i+1] = prefix_sum[i] + sequence[i];
+    while(!file.eof()){ //read the sequence and calculate the prefix sum
+        for(int i=0; i<N; i++){ //read the sequence
+            file>>sequence[i]; //store the sequence
+            prefix_sum[i+1] = prefix_sum[i] + sequence[i]; //calculate the prefix sum
         }
     }
 
@@ -30,18 +30,18 @@ int main(int argc, char* argv[]){
 
     //solution of (O(n^2)) | 2 FOR LOOPS
 
-    for (int i=1; i<=N; i++){
-        for (int j=i; j<N; j++){
-            long long subsequence_sum = prefix_sum[j] - prefix_sum[i-1];
-            long long non_subsequence_sum = totalSum - subsequence_sum;
-            long long difference = abs(subsequence_sum - non_subsequence_sum);
-            if (difference < minSum){
-                minSum = difference;
+    for (int i=1; i<=N; i++){ //start from 1 because of the prefix sum
+        for (int j=i; j<N; j++){ //start from i because of the subsequence
+            long long subsequence_sum = prefix_sum[j] - prefix_sum[i-1]; //calculate the subsequence sum
+            long long non_subsequence_sum = totalSum - subsequence_sum; //calculate the non-subsequence sum
+            long long difference = abs(subsequence_sum - non_subsequence_sum); //calculate the difference
+            if (difference < minSum){ //if the difference is smaller than the minSum, update the minSum
+                minSum = difference; //update the minSum
             }
         }
     }
 
-    cout<<minSum<<endl;
+    cout<<minSum<<endl; //print the minSum
 
 
 
