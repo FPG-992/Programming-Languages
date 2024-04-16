734d959570256169a66c6ac42b155785620c7604
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-04-16 15:12:25 +0300

    Added comments to the code

--- 1st_Set/C++/exercise_1.cpp
+++ 1st_Set/C++/exercise_1.cpp
@@ -15,8 +15,9 @@ int main(int argc, char* argv[]){
     
     file>>N;
 
-    vector<int> sequence(N); //initialize the sequence with N elements
-    vector<long long> prefix_sum(N+1,0); //initialize the prefix sum with 0
+    vector<int> sequence(N); //initialize the sequence with N elements to store the numbers
+    vector<long long> prefix_sum(N+1,0); //initialize the prefix sum with 0 to store the prefix sum
+    //y3 = x3 + x2 + x1 + x0 ....
 
     while(!file.eof()){ //read the sequence and calculate the prefix sum
         for(int i=0; i<N; i++){ //read the sequence
@@ -25,13 +26,16 @@ int main(int argc, char* argv[]){
         }
     }
 
-    long long totalSum = prefix_sum[N]; //total sum of the sequence
-    long long minSum = totalSum; //initialize the minSum with the total sum
+    long long totalSum = prefix_sum[N]; //total sum of the sequence, last element of the prefix sum
+    long long minSum = totalSum; //initialize the minSum with the total sum, because the maximum difference can't be bigger than the total sum
 
     //solution of (O(n^2)) | 2 FOR LOOPS
+    /* Nested loop to check for all possible subsequence sums and non-subsequence sums
+    
+    */
 
-    for (int i=1; i<=N; i++){ //start from 1 because of the prefix sum
-        for (int j=i; j<N; j++){ //start from i because of the subsequence
+    for (int i=1; i<=N; i++){ 
+        for (int j=i; j<N; j++){ 
             long long subsequence_sum = prefix_sum[j] - prefix_sum[i-1]; //calculate the subsequence sum
             long long non_subsequence_sum = totalSum - subsequence_sum; //calculate the non-subsequence sum
             long long difference = abs(subsequence_sum - non_subsequence_sum); //calculate the difference
@@ -42,14 +46,6 @@ int main(int argc, char* argv[]){
     }
 
     cout<<minSum<<endl; //print the minSum
-
-
-
-
-
-
-
-
 
     //close the file
     file.close();
