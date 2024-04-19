ecabfbc5bf4a2e9c43be7cb8df416f879b5e4e31
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-04-19 15:12:45 +0300

    Added code from lab

--- Lab_Prolog/goat.pl
+++ Lab_Prolog/goat.pl
@@ -1,33 +1,31 @@
-/* c
-** config(Man,Wolf,Goat,Cabbage). 
+/*
+** The configuations are represented using terms of the following form:
+**   config(Man, Wolf, Goat, Cabbage)
+** where each of the positions are atoms from the set {w,e}.
 */
 
- /* Thelume na kanoume mia anazhthsh se ena dendro katastasewn 
- **arithmos elaxistwn vhmatos, mia seira apo moves gia na ftasw sthn telikh katastash
- */
-
+/* goat_puzzle(?Moves) */
 goat_puzzle(Moves) :- initial(C), length(Moves, _), solve(C, Moves).
 
-initial(config(w,w,w,w)).
-final(config(e,e,e,e)).
+initial(config(w, w, w, w)).
+final(config(e, e, e, e)).
 
 /* solve(+Configuration, -MoveList) */
-solve(Config,[]) :- final(Config).
-solve(Config, [Move|Moves]) :- 
-safe(NextConfig),
-move(Config,Move,NextConfig),
-solve(NextConfig, Moves).
+solve(Config, []) :- final(Config).
+solve(Config, [Move|Moves]) :-
+   move(Config, Move, NextConfig),
+   safe(NextConfig),
+   solve(NextConfig, Moves).
 
-move(config(A,A,G,C), Wolf, config(B,B,G,C)) :- opposite(A,B).
-move(config(A,W,A,C), Goat, config(B,W,B,C)) :- opposite(A,B).
-move(config(A,W,G,A), Cabbage, config(B,W,G,B)) :- opposite(A,B).
-move(config(A,W,G,C), Nothing, config(B,W,G,C)) :- opposite(A,B).
+move(config(A,A,G,C), wolf, config(B,B,G,C)) :- opposite(A, B).
+move(config(A,W,A,C), goat, config(B,W,B,C)) :- opposite(A, B).
+move(config(A,W,G,A), cabbage, config(B,W,G,B)) :- opposite(A, B).
+move(config(A,W,G,C), nothing, config(B,W,G,C)) :- opposite(A, B).
 
+opposite(e, w).
+opposite(w, e).
 
-opposite(e,w).
-opposite(w,e).
-
-safe(Config(M,W,G,C)) :- together_or_opposite(M,W,G), together_or_seperated(M,G,C).
+safe(config(M,W,G,C)) :- together_or_opposite(M,W,G), together_or_opposite(M,G,C).
 
 together_or_opposite(X,X,X).
 together_or_opposite(_,X,Y) :- opposite(X,Y).
