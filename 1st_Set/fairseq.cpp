59727c7bab23c3a53b3565ba367861b62cba0da8
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-04-30 18:33:21 +0300

    Created structure to read from file

--- 1st_Set/fairseq.cpp
+++ 1st_Set/fairseq.cpp
@@ -4,12 +4,36 @@
 
 using namespace std;
 
+#define MAX_SIZE 1000000
+
 int main(int argc, char* argv[]){
 
+int N; // number of elements in the array
+
+int numbers[MAX_SIZE]; // array of numbers
+
+ifstream file(argv[1]); // open the file
+
+if (!file.is_open()){
+    cout << "Error opening file" << endl;
+    return 1;
+}else {
+    file>>N;
+}
+
+for (int i=0; i<N; i++){
+    file>>numbers[i];
+}
+
+//wrea mexri edw exume diavasei ta dedomena apo to arxeio
+
+for (int i=0; i<N; i++){
+    cout << numbers[i] << " ";
+}
 
 
 
 
-
+file.close(); // close the file
 
 }
