507054d23f56956fdd05dfac296c1044cc4a294a
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-04-30 23:28:46 +0300

    Solution

--- 1st_Set/fairseq.cpp
+++ 1st_Set/fairseq.cpp
@@ -2,8 +2,7 @@
 #include <fstream>
 #include <cmath>
 
-//this problem is very similar to the subset sum problem but with a few differences
-//we will use dp to find the absolute difference of of S' and S
+//this problem is very similar to the subset sum problem 
 
 using namespace std;
 
@@ -14,6 +13,9 @@ int main(int argc, char* argv[]){
 int N; // number of elements in the array
 
 int numbers[MAX_SIZE]; // array of numbers
+
+int total_sum = 0; // total sum of the array
+
 
 ifstream file(argv[1]); // open the file
 
@@ -26,13 +28,31 @@ if (!file.is_open()){
 
 for (int i=0; i<N; i++){
     file>>numbers[i];
+    total_sum += numbers[i];
 }
 
-//we've read the file till here. Now we will start the dp process
+bool dp[total_sum+1] = {false};
+dp[0] = true; // can always have a sum of 0 (using empty list)
+
+for (int i=0; i<N; i++){
+    for (int j=total_sum; j>=numbers[i]; j--){
+        dp[j] = dp[j] or dp[j-numbers[i]]; //ekmetaleuomaste thn idiothta pou uparxei sto provlima (subset sum problem) source: video
+    }
+}
+
+int closest = 0;
+int target = total_sum/2;
+
+for (int i=target; i>=0; i--){ //me to pou vriskei to pio kontino, spaei to loop, den uparxei logos gia parapanw search
+    if (dp[i]==1){
+        closest = i; 
+        break;
+    }
+}
+
+cout<<abs(2*closest - total_sum)<<endl;
 
 
-
-
-file.close(); // close the file
+return 0;
 
 }
